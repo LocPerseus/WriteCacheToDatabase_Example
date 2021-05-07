81fce666ac2a1ea717f26cec11b0078407e15c4b
Loc Hoang <kinghoangloc@gmail.com>
2021-05-07 15:21:53 +0700

    Add project files.

---
+++ Example1/MyCache.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Example1
+{
+    public class MyCache<TKey, TValue>
+    {
+        // type to store datetime and a value
+        private struct CacheItem
+        {
+            public DateTime RetreivedTime;
+            public TValue Value;
+        }
+
+        // the dictionary
+        Dictionary<TKey, CacheItem> _cache = new Dictionary<TKey, CacheItem>();
+
+        private TimeSpan _timeout;
+        private Func<TKey, TValue> _resolveFunc;
+
+        public MyCache(TimeSpan timeout, Func<TKey, TValue> resolveFunc)
+        {
+            _timeout = timeout;
+            _resolveFunc = resolveFunc;
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                CacheItem valueWithDateTime;
+
+                var getNewValue = true;
+
+                if(_cache.TryGetValue(key, out valueWithDateTime))
+                {
+                    getNewValue = valueWithDateTime.RetreivedTime.Add(_timeout) > DateTime.UtcNow;
+                }
+
+                // was it found? or expried?
+                if (getNewValue)
+                {
+                    valueWithDateTime = new CacheItem {
+                        RetreivedTime = DateTime.UtcNow,
+                        Value = _resolveFunc(key)
+                    };
+                    _cache[key] = valueWithDateTime;
+                }
+
+                return valueWithDateTime.Value;
+            }
+        }
+
+        // cleanup
+        public void CleanUp()
+        {
+            var currentDateTime = DateTime.UtcNow;
+
+            // ToArray prevents modifying an iterated collection
+            foreach (var keyValue in _cache.ToArray())
+            {
+                if(keyValue.Value.RetreivedTime.Add(_timeout) < currentDateTime)
+                {
+                    _cache.Remove(keyValue.Key);
+                }
+            }
+        }
+    }
+}
---
+++ Example1/Program.cs
@@ -0,0 +1,35 @@
+﻿using Example1.DAL;
+using Example1.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Program
+    {
+        public static string Data(string t)
+        {
+            DbContext dbContext = new DbContext();
+            var userInfo = dbContext.GetInfoUser();
+            string userName = userInfo.First().Name;
+
+            return userName;
+        }
+
+        static void Main(string[] args)
+        {
+            var usersCache = new NaiveCache<User>();
+
+            var userNameCache = new MyCache<string, string>(TimeSpan.FromMilliseconds(10000),  Data);
+
+            Console.WriteLine("first: " + userNameCache["UserName"]);
+            Task.Delay(12000).Wait();
+            Console.WriteLine("last: " + userNameCache["UserName"]);
+
+            //usersListCache.CleanUp();
+            Console.ReadLine();
+        }
+    }
+}
+ 
